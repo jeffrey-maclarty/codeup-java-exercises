9919a614b0da02995930ab0e9ba0f6d6bb120b5b
Jeffrey MacLarty <jeffrey.maclarty@gmail.com>
2022-04-08 16:02:10 -0400

    Update

---
+++ src/HighLow.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class HighLow {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        byte randomNumber = (byte) ((Math.random() * (100 - 1)) + 1);
+        byte userGuess = 0;
+        byte numberOfGuesses = 0;
+
+        while (userGuess != randomNumber) {
+            System.out.println("Try to guess the number '1 - 100': ");
+            userGuess = scanner.nextByte();
+                if (userGuess == randomNumber) {
+                    System.out.print("Correct! " + userGuess + " is the correct number.");
+                    userGuess = randomNumber;
+                } else if (userGuess > randomNumber) {
+                    System.out.println("Wrong! " + userGuess + " is too high, try a lower number.");
+                } else {
+                    System.out.println("Wrong! " + userGuess + " is too low, try a higher number.");
+                }
+        }
+//        System.out.println(randomNumber);
+    }
+}
